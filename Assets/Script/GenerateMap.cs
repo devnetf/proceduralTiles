dd8586ebe7a35ae60910e7fdd5d004f0ad865a90
Shao Hang He <shaohanghe91@gmail.com>
2015-01-16 22:00:52 -0500

    init

---
+++ Assets/Script/GenerateMap.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class GenerateMap : MonoBehaviour {
+
+	public GameObject TilePrefab;
+	public int maxNumberTile;
+	public int tilesToRemove;
+
+	private Graph map;
+	private List<Node> unvisited_vertices;
+	private System.Random rand = new System.Random();
+	// Use this for initialization
+	void Start () {
+
+		Node firstNode = new Node (new Vector2 (0, 0));
+		map = new Graph (firstNode, null);
+		unvisited_vertices = new List<Node>();
+		unvisited_vertices.Add(firstNode);
+
+		while(map.vertices.Count < maxNumberTile)
+		{
+			Node curr = unvisited_vertices[0];
+
+			Node up = new Node(new Vector2(curr.point.x + 1, curr.point.y));
+			Node down = new Node(new Vector2(curr.point.x-1, curr.point.y));
+			Node leftup = new Node(new Vector2(curr.point.x + 0.5f, curr.point.y + 0.75f));
+			Node rightup = new Node(new Vector2(curr.point.x + 0.5f, curr.point.y - 0.75f));
+			Node leftdown = new Node(new Vector2(curr.point.x - 0.5f, curr.point.y + 0.75f));
+			Node rightdown = new Node(new Vector2(curr.point.x - 0.5f, curr.point.y - 0.75f));
+
+			unvisited_vertices.RemoveAt(0);
+
+			insertNode(curr, up);
+			insertNode(curr, down);
+			insertNode(curr, leftup);
+			insertNode(curr, rightup);
+			insertNode(curr, leftdown);
+			insertNode(curr, rightdown);
+		}
+
+		int nodeRemoved = 0;
+		int index = 0, count = 0;
+
+		while(nodeRemoved < tilesToRemove)
+		{
+			if(count > 1000)
+			{
+				//avoid infinite loop
+				break;
+			}
+
+			index = rand.Next(map.vertices.Count);
+
+			Node curr = map.vertices[index];
+			bool remove = true;
+
+			foreach(Node neighboor in curr.neighbours)
+			{
+				if(neighboor.neighbours.Count <= 1)
+				{
+					remove = false;
+					break;
+				}
+			}
+
+			if(remove)
+			{
+				foreach(Node neighboor in curr.neighbours)
+				{
+					neighboor.neighbours.Remove(curr);
+				}
+
+				map.vertices.Remove(curr);
+				nodeRemoved++;
+			}
+
+			count++;
+		}
+
+		foreach(Node n in map.vertices)
+		{
+			Instantiate(TilePrefab, new Vector3(n.point.x, 0, n.point.y), TilePrefab.transform.rotation);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void insertNode(Node curr, Node n)
+	{
+		if(map.addNodeUnique(n))
+		{
+			unvisited_vertices.Add(n);
+			curr.addNeighbour(n);
+		}
+		else
+		{
+			Node tmpNode = map.GetNode(n.point.x, n.point.y);
+			curr.addNeighbour(tmpNode);
+		}
+	}
+
+}
---
+++ Assets/Script/Graph.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+//Node Data Structure for building Graphs
+public class Node
+{
+	public Vector2 point;
+	public List<Node> neighbours;
+	
+	public Node()
+	{
+		this.point = new Vector2();
+		neighbours = new List<Node>();
+	}
+	
+	public Node(Vector2 pt)
+	{
+		this.point = pt;
+		neighbours = new List<Node>();
+	}
+	
+	public void addNeighbour(Node node)
+	{
+		if(this.neighbours.Where(n => n.point.x == node.point.x && n.point.y == node.point.y).Count() == 0)
+		{
+			this.neighbours.Add(node);
+		}
+	}
+}
+
+//The Graph Class used to store the maze as a grid
+public class Graph
+{
+	
+	public List<Node> vertices;
+	public Node startNode; 
+	public Node Goal;
+	
+	public Graph()
+	{
+		this.vertices = new List<Node>();
+	}
+	
+	public Graph(Node firstNode, Node endNode)
+	{
+		this.vertices = new List<Node>();
+		this.startNode = firstNode;
+		this.Goal = endNode;
+		this.vertices.Add(firstNode);
+		
+	}
+
+	public bool addNodeUnique(Node n)
+	{
+		if (!this.Contains(n.point.x, n.point.y)) 
+		{
+			this.vertices.Add(n);
+			return true;
+		}
+
+		return false;
+	}
+	
+	public bool Contains(float x, float y)
+	{
+		return this.vertices.Where(node => (node.point.x == x && node.point.y == y)).Count() > 0;
+	}
+	
+	public Node GetNode(float x, float y)
+	{
+		return this.vertices.Where(node => (node.point.x == x && node.point.y == y)).FirstOrDefault();
+	}
+	
+	public bool isGoal(Node n)
+	{
+		return n.point.x == this.Goal.point.x && n.point.y == this.Goal.point.y;
+	}
+	
+	public bool isStart(Node n)
+	{
+		return n.point.x == this.startNode.point.x && n.point.y == this.startNode.point.y;
+	}
+}
